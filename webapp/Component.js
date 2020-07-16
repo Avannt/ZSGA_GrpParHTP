12d4775463fab55a0fd568e2640098201e5aaed7
Diego Djeri dos Santos <diego.djeri@live.com>
2020-07-16 19:40:37 +0000

    Up

---
+++ webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function (AppComponent) {
+	return AppComponent.extend("sga.zsgagrppar.Component", {
+		metadata: {
+			"manifest": "json"
+		}
+	});
+});
